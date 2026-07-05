d95247115e6e1bd3c0af414db55b0c8450d67f22
Tom Wiesing <tkw01536@gmail.com>
2026-07-05 08:54:35 +0200

    Remove old auth code (in preparation for reworked auth system)

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,7 +3,6 @@ package cmd
 //spellchecker:words context encoding json flag slog http signal strconv strings syscall time github quickpid auth backend server
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"log/slog"
@@ -17,7 +16,6 @@ import (
 	"time"
 
 	quickpid "github.com/tkw1536/bicpid"
-	"github.com/tkw1536/bicpid/auth"
 	"github.com/tkw1536/bicpid/backend"
 	"github.com/tkw1536/bicpid/server"
 )
@@ -132,8 +130,6 @@ func (main *mainCmd) parseFlags() error {
 	flag.BoolVar(&main.disableSwagger, "disable-swagger", main.disableSwagger, "disable swagger UI and spec file being served")
 	flag.BoolVar(&main.disableInfo, "disable-info", main.disableInfo, "disable info endpoint")
 
-	flag.StringVar(&main.basicAuthFile, "basic-auth-file", main.basicAuthFile, "path to HTTP Basic Authentication file")
-
 	flag.Int64Var(&main.limits.MaxBodyBytes, "max-body-bytes", main.limits.MaxBodyBytes, "maximum size of request body")
 	flag.IntVar(&main.limits.DefaultPageLimit, "default-page-limit", main.limits.DefaultPageLimit, "default number of items per page")
 	flag.IntVar(&main.limits.MaxPageLimit, "max-page-limit", main.limits.MaxPageLimit, "maximum number of items per page")
@@ -211,48 +207,8 @@ func (main *mainCmd) newServerHandler(b backend.Backend) *server.Handler {
 	)
 }
 
-func (main *mainCmd) readAuthFile() (auth.Authorization, error) {
-	if main.basicAuthFile == "" {
-		main.logger.Info("authorization disabled")
-		return auth.NoAuthentication{}, nil
-	}
-
-	data, err := os.ReadFile(main.basicAuthFile)
-	if err != nil {
-		return nil, fmt.Errorf("read auth file %q: %w", main.basicAuthFile, err)
-	}
-
-	var authorization auth.BasicAuthorization
-	if err := json.Unmarshal(data, &authorization); err != nil {
-		return nil, fmt.Errorf("parse auth file %q: %w", main.basicAuthFile, err)
-	}
-
-	main.logger.Info(
-		"loaded authorization",
-		slog.String("auth_file", main.basicAuthFile),
-		slog.Int("users", len(authorization.Authentication.Users)),
-		slog.Int("superusers", len(authorization.Superusers)),
-		slog.Int("namespaces", len(authorization.Permissions)),
-	)
-
-	return &authorization, nil
-}
-
 func (main *mainCmd) serve(h http.Handler) int {
-	authorization, err := main.readAuthFile()
-	if err != nil {
-		main.logger.Error("failed to read auth file", slog.Any("error", err))
-		return 1
-	}
-
-	proxy := auth.Proxy{
-		Forward: func(w http.ResponseWriter, r *http.Request) {
-			h.ServeHTTP(w, r)
-		},
-		Logger: main.logger,
-		Auth:   authorization,
-	}
-	http.Handle(main.mountPath+"/", http.StripPrefix(main.mountPath, &proxy))
+	http.Handle(main.mountPath+"/", http.StripPrefix(main.mountPath, h))
 
 	srv := &http.Server{
 		Addr:    main.addr,
